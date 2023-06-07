32b6a9d1ddec00b0a676b6782c433b7e2c371353
correa-coder <guilhermecorrea903723@gmail.com>
2023-06-07 18:50:56 -0300

    Write tests for extracting links from google drive and mega

--- tests.py
+++ tests.py
@@ -30,7 +30,24 @@ class ParserTests(unittest.TestCase):
         self.assertEqual(VoiceModelParser.extract_name('Maeve (From Paladins) RVC 1.6k Epoch'), 'Maeve (From Paladins)')
         self.assertEqual(VoiceModelParser.extract_name('Britney Spears 100k', is_rvc=False), 'Britney Spears')
         self.assertEqual(VoiceModelParser.extract_name('Britney Spears', is_rvc=False), 'Britney Spears')
-        
+
+    def test_extract_links(self):
+        sample1 = 'Here is the link https://drive.google.com/file/d/1Rn7dOfD3BvZjUP36pAEv8VCYJC9clogS/view?usp=sharing'
+        sample1_links = ['https://drive.google.com/file/d/1Rn7dOfD3BvZjUP36pAEv8VCYJC9clogS/view?usp=sharing']
+        self.assertEqual(VoiceModelParser.extract_links(sample1), sample1_links)
+
+        sample2 = 'Model: https://drive.google.com/file/d/1Rn7dOfD3BvZjUP36pAEv8VCYJC9clogS/view?usp=sharing\nDataset: https://drive.google.com/file/d/1Rn7dOfD3BvZjUP36pAEv8VCYJC9clogS/view?usp=sharing'
+        sample2_links = [
+            'https://drive.google.com/file/d/1Rn7dOfD3BvZjUP36pAEv8VCYJC9clogS/view?usp=sharing',
+            'https://drive.google.com/file/d/1Rn7dOfD3BvZjUP36pAEv8VCYJC9clogS/view?usp=sharing'
+        ]
+        self.assertEqual(VoiceModelParser.extract_links(sample2), sample2_links)
+
+        sample3 = 'Link 1 (MEGA): https://mega.nz/file/1fUjzTYQ#wrCJhg-r0LpRdYQJfuqao_f7JN6phJFQjrTpNlY_aVo'
+        sample3_links = [
+            'https://mega.nz/file/1fUjzTYQ#wrCJhg-r0LpRdYQJfuqao_f7JN6phJFQjrTpNlY_aVo'
+        ]
+        self.assertEqual(VoiceModelParser.extract_links(sample3), sample3_links)
 
 if __name__ == '__main__':
     unittest.main()
